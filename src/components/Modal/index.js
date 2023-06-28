d88443649da286a0f66c6eea3a9a776fd60075f4
Peteranny <peteranny2@gmail.com>
2023-06-29 00:40:11 +0800

    CloseButton to the modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Route } from "react-router-dom";
 import cn from "classnames";
+import usePush from "../../hooks/usePush";
 import styles from "./styles.css";
 
 const Modal = ({ className, path, children }) => {
+  const push = usePush();
+  const close = useCallback(() => {
+    push("..");
+  }, [push]);
+
   return (
     <Route path={path}>
-      <div className={styles.overlay}>
+      <div className={styles.overlay} onClick={close}>
         <div className={styles.container}>
           <div className={cn(styles.modal, className)}>
             <div className={styles.content}>{children}</div>
+            <button className={styles.closeButton} onClick={close} />
           </div>
         </div>
       </div>
