48661a77f9477647791381d2469a50f67361165c
Peteranny <peteranny2@gmail.com>
2023-06-29 00:15:16 +0800

    NotificaitonModal: auto close on 0 notifications

--- src/components/Modal/NotificationModal/index.js
+++ src/components/Modal/NotificationModal/index.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouteMatch } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Modal from "..";
 import Shelf from "./Shelf";
 import Item from "./Item";
-import { unreadBells as unreadBellsSelector } from "../../../store/notifications/selectors";
+import {
+  hasUnreadBells,
+  unreadBells as unreadBellsSelector,
+} from "../../../store/notifications/selectors";
+import usePush from "../../../hooks/usePush";
 
 const Content = () => {
+  const { path } = useRouteMatch();
+  const open = path === "/notification";
+  const unread = useSelector(hasUnreadBells);
   const unreadBells = useSelector(unreadBellsSelector);
+  const push = usePush();
+
+  useEffect(() => {
+    // Close the model when no items
+    if (open && !unread) {
+      push("..");
+    }
+  }, [open, unread, push]);
 
   return (
     <Shelf>
